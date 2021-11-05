f312ab1b82d1b1a4eb3e9e116d3c50d03241afe1
adria <git@adria.link>
2021-11-05 19:47:44 +0800

    refactor: don't use second callback

--- app/routes/jobs.js
+++ app/routes/jobs.js
@@ -1,52 +1,44 @@
 const router = rootRequire('app/router.js');
 const { loadJobs } = rootRequire('app/airtable.js');
 
-router.get(
-  '/jobs.json',
-  (ctx, next) => {
-    ctx.jobs = [];
-    return loadJobs().then((jobs) => {
-      ctx.jobs = jobs;
-      return next();
-    });
-  },
-  (ctx) => (ctx.body = ctx.jobs)
-);
+router.get('/jobs.json', (ctx, next) => {
+  ctx.jobs = [];
+  return loadJobs().then((jobs) => {
+    ctx.body = jobs;
+    return next();
+  });
+});
 
-router.post(
-  '/jobs/search.json',
-  (ctx, next) => {
-    const data = ctx.request.body;
+router.post('/jobs/search.json', (ctx, next) => {
+  const data = ctx.request.body;
 
-    ctx.jobs = [];
-    return loadJobs().then((jobs) => {
-      const filterJobTitle = (data.job_title && data.job_title.toLowerCase()) || '~$%_e23'; // We add some garbage so nothing matches
-      const filterSkills = data.skills || [];
+  ctx.jobs = [];
+  return loadJobs().then((jobs) => {
+    const filterJobTitle = (data.job_title && data.job_title.toLowerCase()) || '~$%_e23'; // We add some garbage so nothing matches
+    const filterSkills = data.skills || [];
 
-      // Filter the returning jobs matching criteria
-      ctx.jobs = jobs.filter((job) => {
-        let matches = false;
+    // Filter the returning jobs matching criteria
+    ctx.body = jobs.filter((job) => {
+      let matches = false;
 
-        if (
-          // Job title
-          job.title.toLowerCase().indexOf(filterJobTitle) > -1 ||
-          // Alternative names
-          (job.alternative_names !== undefined && job.alternative_names.toLowerCase().indexOf(filterJobTitle) > -1)
-        ) {
+      if (
+        // Job title
+        job.title.toLowerCase().indexOf(filterJobTitle) > -1 ||
+        // Alternative names
+        (job.alternative_names !== undefined && job.alternative_names.toLowerCase().indexOf(filterJobTitle) > -1)
+      ) {
+        matches = true;
+      }
+      if (matches === true) return true;
+
+      filterSkills.forEach((skill) => {
+        if ((job.skills + '').toLowerCase().indexOf(skill.toLowerCase()) > -1) {
           matches = true;
         }
-        if (matches === true) return true;
-
-        filterSkills.forEach((skill) => {
-          if ((job.skills.toLowerCase() + '').indexOf(skill.toLowerCase()) > -1) {
-            matches = true;
-          }
-        });
-
-        return matches;
       });
-      return next();
+
+      return matches;
     });
-  },
-  (ctx) => (ctx.body = ctx.jobs)
-);
+    return next();
+  });
+});
