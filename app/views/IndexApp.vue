53eb0c8539aa0fab3b28ff8387e17e41bae1e48e
adria <git@adria.link>
2021-11-06 17:55:40 +0800

    wip: second step

--- app/views/IndexApp.vue
+++ app/views/IndexApp.vue
@@ -21,22 +21,28 @@
         <div v-else>You are lucky!! Your job will unlikely be automated by 2030.</div>
         <h3>2. And which skills from {{ currentJob.title }} do you master?</h3>
         <ul v-for="skill in currentJob.skills" :key="skill">
-          <li>{{ skill }}</li>
+          <li>
+            {{ skill }}
+            <fancy-check @check="skillCheck" :passthrough="skill" />
+          </li>
         </ul>
+        <button class="bg-green-800" @click="step2Click">Continue Please!</button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import FancyCheck from './components/FancyCheck.vue';
 import JobSearch from './components/JobSearch.vue';
 
 export default {
-  components: { JobSearch },
+  components: { JobSearch, FancyCheck },
   data() {
     return {
       currentStep: 1,
       currentJob: null,
+      currentSkills: new Set(),
     };
   },
   mounted() {
@@ -45,10 +51,19 @@ export default {
   methods: {
     jobSelected(job) {
       this.currentJob = job;
-      this.currentStep = 2;
     },
     step1Click() {
-      console.log('click');
+      this.currentStep = 2;
+    },
+    skillCheck(checked, skillName) {
+      if (checked) {
+        this.currentSkills.add(skillName);
+      } else {
+        this.currentSkills.delete(skillName);
+      }
+    },
+    step2Click() {
+      this.currentStep = 3;
     },
   },
 };
---
+++ app/views/components/FancyCheck.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if="checked === false" @click="mainClick">unselected</div>
+    <div v-else @click="mainClick">selected</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['passthrough'],
+  data() {
+    return {
+      checked: false,
+    };
+  },
+  methods: {
+    mainClick() {
+      this.checked = !this.checked;
+      this.$emit('check', this.checked, this.passthrough);
+    },
+  },
+};
+</script>
