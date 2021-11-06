f294b68f9c78b55ca94292c03b7fd994a946cdf8
adria <git@adria.link>
2021-11-06 16:59:45 +0800

    feat: allow requests from the FE

--- app/init.js
+++ app/init.js
@@ -47,6 +47,12 @@ function start() {
         trustHostHeader: true,
       })
     )
+    .use(async (ctx, next) => {
+      ctx.set('Access-Control-Allow-Origin', '*');
+      ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+      await next();
+    })
     .use(cacheControl({ noCache: true }))
     .use(json())
     .use(conditionalGet())
