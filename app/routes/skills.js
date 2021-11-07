02be9798a8e2761cb61fa3c09ca140964f9c0457
adria <git@adria.link>
2021-11-07 21:10:10 +0800

    refactor: simplify code

--- app/routes/skills.js
+++ app/routes/skills.js
@@ -1,19 +1,15 @@
 const router = rootRequire('app/router.js');
 const { loadJobs } = rootRequire('app/airtable.js');
 
-router.get(
-  '/skills.json',
-  (ctx, next) => {
-    ctx.jobs = [];
-    return loadJobs().then((jobs) => {
-      const skills = new Set();
-      jobs.forEach((job) => {
-        if (undefined === job.skills) return;
-        job.skills.forEach((skill) => skills.add(skill.toLowerCase()));
-      });
-      ctx.skills = [...skills];
-      return next();
+router.get('/skills.json', (ctx, next) => {
+  ctx.jobs = [];
+  return loadJobs().then((jobs) => {
+    const skills = new Set();
+    jobs.forEach((job) => {
+      if (undefined === job.skills) return;
+      job.skills.forEach((skill) => skills.add(skill.toLowerCase()));
     });
-  },
-  (ctx) => (ctx.body = ctx.skills)
-);
+    ctx.body = [...skills];
+    return next();
+  });
+});
