73ae97a755c9a715ae965c84791e8e35b8ebcbb5
adria <git@adria.link>
2021-11-07 19:42:41 +0800

    feat: adds final result

--- app/views/IndexApp.vue
+++ app/views/IndexApp.vue
@@ -2,7 +2,7 @@
   <div v-cloak>
     <form @submit.prevent>
       <div v-if="[1, 2].indexOf(currentStep) > -1">
-        <h3 class="text-5xl mt-20 text-center">1. Tell us about your current job</h3>
+        <h3 class="text-5xl mt-20">1. Tell us about your current job</h3>
         <div class="grid grid-cols-2 gap-16 mt-10">
           <label>
             <span class="pl-4 text-sm">Current job title</span>
@@ -53,9 +53,12 @@
           <div v-else-if="currentJob.future_demand > 3 && currentJob.future_demand < 5">
             Jor job will continue to be demanded. Your job has some changes to get automated in 2030.
           </div>
-          <div v-else>You are lucky!! Your job will unlikely be automated by 2030.</div>
+          <div v-else>
+            You are lucky!! Your job will unlikely be automated by 2030. Still, you can look around to see what the
+            future looks like.
+          </div>
         </div>
-        <h3 class="text-5xl mt-20 text-center">2. And which skills from {{ currentJob.title }} do you master?</h3>
+        <h3 class="text-5xl mt-20">2. And which skills from {{ currentJob.title }} do you master?</h3>
         <ul class="grid grid-cols-2 gap-16 mt-14">
           <li v-for="skill in currentJob.skills" :key="skill" class="flex justify-between">
             <div class="flex justify-between w-full">
@@ -84,101 +87,40 @@
       </div>
 
       <div v-if="currentStep === 3">
-        <h3 class="text-5xl mt-20 text-center">These are your future possibilities!</h3>
+        <h3 class="text-5xl mt-20">These are your future possibilities!</h3>
         <ul class="grid grid-cols-2 gap-16 content-start mt-14">
-          <li v-for="job in jobMatches" :key="job.id" class="rounded-3xl text-white job-offer-skilled">
-            <div class="flex justify-between w-full mt-5 px-5">
-              <span class="text-2xl">{{ caps(job.title) }}</span>
-              <div class="text-right">
-                <div class="text-4xl font-bold">{{ job.match_percent }}%</div>
-                <div class="text-sm">match</div>
-              </div>
-            </div>
-            <div class="grid grid-cols-2 gap-x-10 mt-5 px-5">
-              <div>
-                <div class="font-bold text-base">2030 Demand</div>
-                <ul class="flex justify-between space-x-4 w-full mt-2">
-                  <li v-for="index in job.future_demand" :key="index">
-                    <img
-                      src="../assets/images/foot-icon.png"
-                      srcset="../assets/images/foot-icon.png 1x, ../assets/images/foot-icon@2x.png 2x"
-                      alt="Star"
-                      class="invert"
-                    />
-                  </li>
-                  <li v-for="index in 5 - job.future_demand" :key="index">
-                    <img
-                      src="../assets/images/foot-icon.png"
-                      srcset="../assets/images/foot-icon.png 1x, ../assets/images/foot-icon@2x.png 2x"
-                      alt="Star"
-                      class="opacity-50 invert"
-                    />
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <div class="font-bold text-base">Salary Potential</div>
-                <ul class="flex justify-between space-x-4 w-full mt-2">
-                  <li v-for="index in job.salary_range" :key="index">
-                    <img
-                      src="../assets/images/foot-icon.png"
-                      srcset="../assets/images/foot-icon.png 1x, ../assets/images/foot-icon@2x.png 2x"
-                      alt="Star"
-                      class="invert"
-                    />
-                  </li>
-                  <li v-for="index in 5 - job.salary_range" :key="index">
-                    <img
-                      src="../assets/images/foot-icon.png"
-                      srcset="../assets/images/foot-icon.png 1x, ../assets/images/foot-icon@2x.png 2x"
-                      alt="Star"
-                      class="opacity-50 invert"
-                    />
-                  </li>
-                </ul>
-              </div>
-              <div class="col-span-2 font-bold text-base mt-5">Skills to Learn</div>
-              <div class="col-span-2 grid grid-cols-2 gap-x-10 skills-learn-styled">
-                <span v-for="skill in job.skills" :key="skill" class="text-lg">{{ caps(skill) }}</span>
-              </div>
-            </div>
-            <div class="w-11/12 mx-auto mb-4 mt-5">
-              <button
-                class="
-                  bg-green-900
-                  border-4 border-green-700 border-solid
-                  rounded-full
-                  py-3
-                  mt-2
-                  w-full
-                  text-white text-2xl
-                  button-extra-style
-                "
-                @click="step3Click"
-                :data-job-id="job.id"
-              >
-                Choose this Path
-              </button>
-            </div>
+          <li v-for="job in jobMatches" :key="job.id">
+            <job-card :job="job" :show-button="true" @job-proceed-click="step3Click" :job-matches="jobMatches" />
           </li>
         </ul>
       </div>
 
       <div v-if="currentStep === 4">
-        <h3 class="text-5xl mt-20 text-center">Welcome future {{ selectedJob.title }}!</h3>
+        <h3 class="text-5xl mt-20">Welcome future {{ selectedJob.title }}!</h3>
+        <div class="grid grid-cols-2 gap-16 mt-8">
+          <div>
+            <p class="mt-5">
+              {{ caps(selectedJob.title) }} is a great future job, with
+              {{ Math.round((selectedJob.future_demand * 100) / 5) }}% chances of not getting obsolete in 2030 and a
+              {{ Math.round((selectedJob.salary_range * 100) / 5) }}% average salary potential. With a {{ jobMatch }}%
+              match you will quickly become a great {{ caps(selectedJob.title) }} to save your future!
+            </p>
+            <job-card :job="selectedJob" :job-matches="jobMatches" class="mt-5" />
+          </div>
+          <img
+            src="../assets/images/gorilla-yay.png"
+            srcset="../assets/images/gorilla-yay.png 1x, ../assets/images/gorilla-yay@2x.png 2x"
+            alt="Gorilla YAY"
+          />
+        </div>
+
+        <h3 class="text-5xl mt-20">Skills School</h3>
         <p class="mt-5">
-          {{ caps(selectedJob.title) }} is a great future job, with
-          {{ Math.round((selectedJob.future_demand * 100) / 5) }} chances of not getting obsolete in 2030 and a
-          {{ Math.round((selectedJob.salary_range * 100) / 5) }} average salary potential. With a {{ jobMatch }}% match
-          you will quickly become a great {{ caps(selectedJob.title) }} to save your future!
+          {{ caps(selectedJob.title) }} requires some additional skills which you are currently missing.
+          {{ missingSkillsSchoolText }}.<br />
+          You can quickly gain those skills if you start as soon as possible with your reeskilling.
         </p>
-        <h3>Skills School</h3>
-        <p>
-          {{ selectedJob.title }} requires some additional skills which you are currently missing.
-          {{ missingSkillsSchoolText }}. You can quickly gain those skills if you start as soon as possible with your
-          reeskilling.
-        </p>
-        <p>Find below some online courses which will help you start your new career path.</p>
+        <p class="mt-5">Find below some online courses which will help you start your new career path.</p>
         <!-- <ul v-for="course in courses" :key="course.id">
           <li>
             {{ course.title }}
@@ -191,10 +133,11 @@
 
 <script>
 import FancyCheck from './components/FancyCheck.vue';
+import JobCard from './components/JobCard.vue';
 import JobSearch from './components/JobSearch.vue';
 
 export default {
-  components: { JobSearch, FancyCheck },
+  components: { JobSearch, FancyCheck, JobCard },
   data() {
     return {
       currentStep: 1,
@@ -258,9 +201,8 @@ export default {
     step2Click() {
       this.currentStep = 3;
     },
-    step3Click(event) {
-      const jobId = event.target.dataset.jobId;
-      this.selectedJob = this.jobMatches.find((x) => x.id === jobId);
+    step3Click(job) {
+      this.selectedJob = job;
       this.currentStep = 4;
     },
   },
---
+++ app/views/components/JobCard.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="rounded-3xl text-white job-offer-skilled p-2">
+    <div class="flex justify-between w-full mt-5 px-5">
+      <span class="text-2xl">{{ caps(job.title) }}</span>
+      <div class="text-right">
+        <div class="text-4xl font-bold">{{ job.match_percent }}%</div>
+        <div class="text-sm">match</div>
+      </div>
+    </div>
+    <div class="grid grid-cols-2 gap-x-10 mt-5 px-5">
+      <div>
+        <div class="font-bold text-base">2030 Demand</div>
+        <ul class="flex justify-between space-x-4 w-full mt-2">
+          <li v-for="index in job.future_demand" :key="index">
+            <img
+              src="../../assets/images/foot-icon.png"
+              srcset="../../assets/images/foot-icon.png 1x, ../../assets/images/foot-icon@2x.png 2x"
+              alt="Star"
+              class="invert"
+            />
+          </li>
+          <li v-for="index in 5 - job.future_demand" :key="index">
+            <img
+              src="../../assets/images/foot-icon.png"
+              srcset="../../assets/images/foot-icon.png 1x, ../../assets/images/foot-icon@2x.png 2x"
+              alt="Star"
+              class="opacity-50 invert"
+            />
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div class="font-bold text-base">Salary Potential</div>
+        <ul class="flex justify-between space-x-4 w-full mt-2">
+          <li v-for="index in job.salary_range" :key="index">
+            <img
+              src="../../assets/images/foot-icon.png"
+              srcset="../../assets/images/foot-icon.png 1x, ../../assets/images/foot-icon@2x.png 2x"
+              alt="Star"
+              class="invert"
+            />
+          </li>
+          <li v-for="index in 5 - job.salary_range" :key="index">
+            <img
+              src="../../assets/images/foot-icon.png"
+              srcset="../../assets/images/foot-icon.png 1x, ../../assets/images/foot-icon@2x.png 2x"
+              alt="Star"
+              class="opacity-50 invert"
+            />
+          </li>
+        </ul>
+      </div>
+      <div class="col-span-2 font-bold text-base mt-5">Skills to Learn</div>
+      <div class="col-span-2 grid grid-cols-2 gap-x-10 skills-learn-styled">
+        <span v-for="skill in job.skills" :key="skill" class="text-lg">{{ caps(skill) }}</span>
+      </div>
+    </div>
+    <div class="w-11/12 mx-auto mb-4 mt-5" v-if="showButton">
+      <button
+        class="
+          bg-green-900
+          border-4 border-green-700 border-solid
+          rounded-full
+          py-3
+          mt-2
+          w-full
+          text-white text-2xl
+          button-extra-style
+        "
+        @click="jobProceedClick"
+        :data-job-id="job.id"
+      >
+        Choose this Path
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['job', 'showButton', 'jobMatches'],
+  data() {
+    return {};
+  },
+  methods: {
+    caps(text) {
+      return text.replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()));
+    },
+    jobProceedClick(event) {
+      const jobId = event.target.dataset.jobId;
+      this.$emit(
+        'jobProceedClick',
+        this.jobMatches.find((x) => x.id === jobId)
+      );
+    },
+  },
+};
+</script>
