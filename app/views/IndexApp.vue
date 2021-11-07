8cbdeeb7b226f54931a2f77d6431ed74e12cbc92
adria <git@adria.link>
2021-11-07 16:30:32 +0800

    feat: styling skills

--- app/views/IndexApp.vue
+++ app/views/IndexApp.vue
@@ -29,28 +29,62 @@
       </div>
 
       <div v-if="currentStep === 2">
-        <ul>
-          <li v-for="index in currentJob.future_demand" :key="index">star</li>
-        </ul>
-        <div v-if="currentJob.future_demand < 3">
-          You like risky things!! Your job has very high change to get automated by 2030.
+        <div class="grid grid-cols-2 gap-16 mt-10">
+          <ul class="flex justify-between space-x-4 pl-4">
+            <li v-for="index in currentJob.future_demand" :key="index">
+              <img
+                src="../assets/images/foot-icon.png"
+                srcset="../assets/images/foot-icon.png 1x, ../assets/images/foot-icon@2x.png 2x"
+                alt="Star"
+              />
+            </li>
+            <li v-for="index in 5 - currentJob.future_demand" :key="index">
+              <img
+                src="../assets/images/foot-icon.png"
+                srcset="../assets/images/foot-icon.png 1x, ../assets/images/foot-icon@2x.png 2x"
+                alt="Star"
+                class="opacity-50"
+              />
+            </li>
+          </ul>
+          <div v-if="currentJob.future_demand < 3">
+            You like risky things!! Your job has a very high change to get automated by 2030.
+          </div>
+          <div v-else-if="currentJob.future_demand > 3 && currentJob.future_demand < 5">
+            Jor job will continue to be demanded. Your job has some changes to get automated in 2030.
+          </div>
+          <div v-else>You are lucky!! Your job will unlikely be automated by 2030.</div>
         </div>
-        <div v-else-if="currentJob.future_demand > 3 && currentJob.future_demand < 5">
-          Jor job will continue to be demanded. Your job has some changes to get automated in 2030.
-        </div>
-        <div v-else>You are lucky!! Your job will unlikely be automated by 2030.</div>
-        <h3>2. And which skills from {{ currentJob.title }} do you master?</h3>
-        <ul>
-          <li v-for="skill in currentJob.skills" :key="skill">
-            {{ skill }}
-            <fancy-check @check="skillCheck" :passthrough="skill" />
+        <h3 class="text-5xl mt-20 text-center">2. And which skills from {{ currentJob.title }} do you master?</h3>
+        <ul class="grid grid-cols-2 gap-16 mt-14">
+          <li v-for="skill in currentJob.skills" :key="skill" class="flex justify-between">
+            <div class="flex justify-between w-full">
+              <span class="text-xl">{{ skill }}</span>
+              <fancy-check @check="skillCheck" :passthrough="skill" />
+            </div>
           </li>
         </ul>
-        <button class="bg-green-800" @click="step2Click" v-if="currentSkills.size > 0">Continue Please!</button>
+        <div v-if="currentSkills.size > 0" class="w-96 mx-auto mt-14">
+          <button
+            class="
+              bg-green-900
+              border-4 border-green-700 border-solid
+              rounded-full
+              py-3
+              mt-2
+              w-full
+              text-white text-2xl
+              button-extra-style
+            "
+            @click="step2Click"
+          >
+            Continue Please!
+          </button>
+        </div>
       </div>
 
       <div v-if="currentStep === 3">
-        <h3>These are your future possibilities!</h3>
+        <h3 class="text-5xl mt-20 text-center">These are your future possibilities!</h3>
         <ul>
           <li v-for="job in jobMatches" :key="job.id">
             {{ job.title }}
