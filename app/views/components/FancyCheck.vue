49472d1b2000ea150ac59b64a8200c0e689c062a
adria <git@adria.link>
2021-11-07 17:41:02 +0800

    feat: style checkbox

--- app/views/components/FancyCheck.vue
+++ app/views/components/FancyCheck.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
-    <div v-if="checked === false" @click="mainClick">unselected</div>
-    <div v-else @click="mainClick">selected</div>
+    <div v-if="checked === false" @click="mainClick" class="cursor-pointer">
+      <img
+        src="../../assets/images/unchecked.png"
+        srcset="../../assets/images/unchecked.png 1x, ../../assets/images/unchecked@2x.png 2x"
+        alt="Unchecked"
+      />
+    </div>
+    <div v-else @click="mainClick" class="cursor-pointer">
+      <img
+        src="../../assets/images/checked.png"
+        srcset="../../assets/images/checked.png 1x, ../../assets/images/checked@2x.png 2x"
+        alt="Checked"
+      />
+    </div>
   </div>
 </template>
 
